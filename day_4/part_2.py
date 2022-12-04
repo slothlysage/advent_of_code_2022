bdd0220a11db0b754d202826a3b0ac58d47acad0
Sean Jones <seanjones2848@gmail.com>
2022-12-03 21:20:37 -0800

    solved in day 4 in python

---
+++ day_4/part_2.py
@@ -0,0 +1,14 @@
+input = open('puzzle_input', 'r')
+sum = 0
+
+for line in input:
+    line = line.strip().split(',')
+    pair_1 = line[0].split('-')
+    pair_2 = line[1].split('-')
+    range_1 = set(range(int(pair_1[0]), int(pair_1[1]) + 1))
+    range_2 = set(range(int(pair_2[0]), int(pair_2[1]) + 1))
+    intersection = range_1 & range_2
+    if (intersection):
+        sum += 1
+
+print(sum)
