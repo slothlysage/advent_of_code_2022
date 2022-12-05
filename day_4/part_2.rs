7ed6c72465a78e04d248fdb8bd8001ed914204d3
Sean Jones <seanjones2848@gmail.com>
2022-12-04 19:25:39 -0800

    refined part 2 a bit more

--- day_4/part_2.rs
+++ day_4/part_2.rs
@@ -13,12 +13,8 @@ fn main() {
         let mut ranges = i.split(",");
         let mut range_1 = ranges.next().unwrap().split("-").map(|x| x.parse::<i32>().unwrap());
         let mut range_2 = ranges.next().unwrap().split("-").map(|x| x.parse::<i32>().unwrap());
-        let range_1_low: i32 = range_1.next().unwrap();
-        let range_1_high: i32 = range_1.next().unwrap();
-        let range_2_low: i32 = range_2.next().unwrap();
-        let range_2_high: i32 = range_2.next().unwrap();
-        let set_1: HashSet<i32> = (range_1_low..=range_1_high).collect::<Vec<i32>>().into_iter().collect();
-        let set_2: HashSet<i32> = (range_2_low..=range_2_high).collect::<Vec<i32>>().into_iter().collect();
+        let set_1: HashSet<i32> = (range_1.next().unwrap()..=range_1.next().unwrap()).collect::<Vec<i32>>().into_iter().collect();
+        let set_2: HashSet<i32> = (range_2.next().unwrap()..=range_2.next().unwrap()).collect::<Vec<i32>>().into_iter().collect();
         let intersect = set_1.intersection(&set_2).collect::<Vec<&i32>>();
         if intersect.len() > 0 {
             sum += 1
